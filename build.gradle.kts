192a0c21c6da691f662271ec6be835b87f6fef00
granny <contact@granny.dev>
2024-05-13 23:24:46 -0700

    re-add minotaur plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     id("java-library")
     id("io.github.goooler.shadow") version "8.1.7" // TODO: Temp
+    id("com.modrinth.minotaur") version "2.+" // TODO: Temp
 }
 
 val buildNum = System.getenv("NEXT_BUILD_NUMBER") ?: "TEMP" // TODO: Temp
@@ -22,5 +23,33 @@ tasks {
 
     build {
         dependsOn(shadowJar)
+    }
+}
+
+modrinth {
+    autoAddDependsOn = false
+    token = System.getenv("MODRINTH_TOKEN")
+    projectId = "pl3xmap"
+    versionName = "${project.version}"
+    versionNumber = "${project.version}"
+    versionType = "beta"
+    uploadFile = rootProject.layout.buildDirectory.file("libs/${rootProject.name}-${project.version}.jar").get()
+    //additionalFiles.addAll([
+    //        rootProject.layout.buildDirectory.file("libs/${rootProject.name}-${project.version}-javadoc.jar").get(),
+    //        rootProject.layout.buildDirectory.file("libs/${rootProject.name}-${project.version}-sources.jar").get()
+    //])
+    gameVersions.addAll(listOf("${rootProject.properties["minecraftVersion"]}"))
+    loaders.addAll(listOf("bukkit", "fabric", /*"forge",*/ "paper", "purpur", "quilt", "spigot", "folia"))
+    changelog = System.getenv("COMMIT_MESSAGE")
+    dependencies {
+        required.project("fabric-api")
+         //optional.project(
+         //    "pl3xmap-banners",
+         //    "pl3xmap-claims",
+         //    "pl3xmap-mobs",
+         //    "pl3xmap-signs",
+         //    "pl3xmap-warps",
+         //    "deathspots",
+         //)
     }
 }
